63d8698a2ecb919296c795939361049afde4b989
erik savage <erik.k.savage@gmail.com>
2026-08-15 11:07:16 -0700

    Update pipes to cave formations

--- scripts/FormationSet.cs
+++ scripts/FormationSet.cs
@@ -1,24 +1,31 @@
 using Godot;
 
 namespace Projects.scripts;
-public partial class PipeSet : Node2D
+public partial class FormationSet : Node2D
 {
 	[Export]
 	public float MoveSpeed;
-	// Called when the node enters the scene tree for the first time.
+
+	[Export]
+	public float FormationGap;
 	
 	private bool _passed;
 	public override void _Ready()
 	{
-		var pipeSpawner = GetParent().GetNode<Node2D>("PipeSpawner");
-		Position = pipeSpawner.Position;
+		var stalactite = GetNode<StaticBody2D>("Stalactite");
+		stalactite.Position = new Vector2(stalactite.Position.X, -FormationGap / 2);
+		
+		var stalagmite = GetNode<StaticBody2D>("Stalagmite");
+		stalagmite.Position = new Vector2(stalagmite.Position.X, FormationGap / 2);
+
+		var formationSpawner = GetParent().GetNode<Node2D>("FormationSpawner");
+		Position = formationSpawner.Position;
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
 		MoveLeft((float)delta);
-		EvaluatePipesPassed();
+		EvaluateFormationPassed();
 			
 		if (GlobalPosition.X < -500)
 		{
@@ -30,7 +37,7 @@ public partial class PipeSet : Node2D
 	{
 		Position = new Vector2(Position.X + (-1 * MoveSpeed * delta), Position.Y);
 	}
-	private void EvaluatePipesPassed()
+	private void EvaluateFormationPassed()
 	{
 		var batPosition = GetParent().GetNode<Node2D>("Bat").GlobalPosition.X;
 		var pipePosition = GlobalPosition.X;
@@ -38,7 +45,7 @@ public partial class PipeSet : Node2D
 		if (!_passed && (batPosition > pipePosition))
 		{
 			var eventBus = GetNode<EventBus>("/root/EventBus");
-			eventBus.EmitPipesPassed();	
+			eventBus.EmitFormationPassed();	
 			_passed = true;
 		}
 
